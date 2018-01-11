47fbfac8d4327f8cdf0a097a1667661a738022f4
zhaosiyuan06 <15735800437@163.com>
2018-01-11 17:07:30 +0800

    电影图文页面编写

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,65 +1,25 @@
 Page({
+//原来通过 SOAP接口获取到 XML
+// 现在RESTFULL API JSON
+onLoad:function(event){
+  wx.request({
+    url: "https://api.douban.com/v2/movie/top250",
+    data:{},
+    method:"GET",
+    header:{
+      "Content-Type": "application/xml"
+    },
+    success:function(res){
+      console.log(res)
+    },
+    fail:function(error){
+      console.log(error)
+    },
+    complete:function(){
+      
+    }
+  })
+}
+  
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
-  }
 })
