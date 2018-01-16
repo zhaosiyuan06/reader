5b63e9a76c69ea30f281baeb421c34c2e33909a6
zhaosiyuan06 <15735800437@163.com>
2018-01-16 21:53:48 +0800

    从豆瓣API获取电影信息

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -2,16 +2,21 @@ var app=getApp();
 Page({
 //原来通过 SOAP接口获取到 XML
 // 现在RESTFULL API JSON
+data:{
+  inTheaters:{},
+  comingSoon:{},
+  top250:{}
+},
 onLoad:function(event){
   var InTheatersUrl = app.globalData.g_UrlData+"/v2/movie/in_theaters"+"?start=0&count=3";
   var ComingSoonUrl = app.globalData.g_UrlData + "/v2/movie/coming_soon"+"?start=0&count=3";
   var Top250Url = app.globalData.g_UrlData + "/v2/movie/top250"+"?start=0&count=3";
-  this.getMovieListData(InTheatersUrl);
-  this.getMovieListData(ComingSoonUrl);
-  this.getMovieListData(Top250Url);
+  this.getMovieListData(InTheatersUrl,"inTheaters");
+  this.getMovieListData(ComingSoonUrl,"comingSoon");
+  this.getMovieListData(Top250Url,"top250");
 },
   
-getMovieListData:function(url){
+  getMovieListData: function (url,settedKey){
   var that=this;
   wx.request({
     url: url,
@@ -20,8 +25,8 @@ getMovieListData:function(url){
       "Content-Type": "application/xml"
     },
     success: function (res) {
-      console.log(res);
-      that.processDoubanData(res.data)
+      // console.log(res);
+      that.processDoubanData(res.data, settedKey)
     },
     fail: function (error) {
       console.log(error)
@@ -31,7 +36,7 @@ getMovieListData:function(url){
     }
   })
 },
-processDoubanData:function(moviesDouban){
+processDoubanData:function(moviesDouban,settedKey){
     var movies=[];
     for(var idx in moviesDouban.subjects){
       var subject = moviesDouban.subjects[idx];
@@ -47,9 +52,11 @@ processDoubanData:function(moviesDouban){
       }
       movies.push(temp);
     }
-    this.setData({
+    var readyData={};
+    readyData[settedKey]={
       movies:movies
-    })
+    }
+    this.setData(readyData);
 
 }
 })
